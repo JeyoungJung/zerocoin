c33eccf9909a8620142abd9416fb91857d0ccaf7
Jeyoung Jung <jeyoung.jung@ontario.ca>
2022-03-30 02:27:40 -0400

    basic blockchain

--- main.go
+++ main.go
@@ -1,14 +1,46 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
-
-	"github.com/jeyoungjung/zerocoin/person"
 )
 
+type block struct {
+	data     string
+	hash     string
+	prevHash string
+}
+
+type blockchain struct {
+	blocks []block
+}
+
+func (b *blockchain) getPrevHash () string {
+	if (len(b.blocks) > 0) {
+		return b.blocks[len(b.blocks)-1].hash
+	}
+	return ""
+}
+
+func (b *blockchain) addBlock (data string) {
+	newBlock := block{data, "", b.getPrevHash()}
+	hash := fmt.Sprintf("%x", sha256.Sum256([]byte(newBlock.data + newBlock.prevHash)))
+	newBlock.hash = hash
+	b.blocks = append(b.blocks, newBlock)
+}
+
+func (b blockchain) listBlockchain () {
+	for _, block := range b.blocks {
+		fmt.Printf("data: %s\n", block.data)
+		fmt.Printf("hash: %s\n", block.hash)
+		fmt.Printf("prev data: %s\n", block.prevHash)
+	}
+}
+
 func main() {
-	jay := person.Person{}
-	jay.SetDetails("jay", 19)
-	fmt.Println("main jay", jay)
-    fmt.Println(jay.Name())
+	blockchain := blockchain{};
+	blockchain.addBlock("Genesis Block")
+	blockchain.addBlock("Second Block")
+	blockchain.addBlock("Third Block")
+	blockchain.listBlockchain()
 }
